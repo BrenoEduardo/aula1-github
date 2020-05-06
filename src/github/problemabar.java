a3215ad9e0f8ac74ad8ac931dfbc4505c1f6f536
BrenoEduardo <brenoeducm@hotmail.com>
2020-05-06 15:22:15 -0300

    Relatorio gerado

--- src/github/problemabar.java
+++ src/github/problemabar.java
@@ -10,8 +10,10 @@ public class problemabar {
 		Scanner sc = new Scanner(System.in);
 		
 		
-		int cerv,refri,espetinho;
+		int cerv,refri,espetinho,valorcerv, valorrefri, valorespet;
 		char sexo;
+		double valortotal, couvert, valorapagar, ingresso;
+		// LEITURA DE DADOS
 		
 		System.out.printf("Sexo : ");
 		sexo = sc.next().charAt(0);
@@ -22,6 +24,43 @@ public class problemabar {
 		System.out.printf("Quantidade de espetinho: ");
 		espetinho = sc.nextInt();
 		
+		// LEITURA DE DADOS
+			valorcerv = cerv * 5;
+			valorrefri = refri * 3;
+			valorespet = espetinho * 7;
+			
+			
+			if(sexo == 'F' || sexo == 'f') {
+				ingresso = 8.0;	
+			}
+			else {
+				ingresso = 10.0;
+			}
+			valortotal = valorespet + valorrefri + valorcerv;
+			if(valortotal > 30) {
+				couvert = 0.0;
+			}
+			else {
+				couvert = 4.0;
+			}
+			valorapagar = valortotal + ingresso + couvert;
+			
+			// RELATORIO
+			
+			System.out.println();
+			System.out.println("RELATÓRIO:");
+			System.out.printf("Consumo = R$ %.2f%n", valortotal);
+			if( couvert == 0.0) {
+				System.out.println("Isento de Couvert");
+			}
+			else {
+				System.out.printf("Couvert = R$ %.2f%n", couvert);
+			}
+			System.out.printf("Ingresso = R$ %.2f%n ", ingresso);
+			System.out.println();
+			
+			System.out.printf("Valor a pagar = R$ %.2f%n" , valorapagar);
+			
 		sc.close();
 	}
 
